1f336d460815f0772220f206fd89da09b123a300
Johan Ejstrud <johan@ejstrud.com>
2021-08-05 13:35:50 -0200

    Recalculate selectable every time buffer is changed
    Closes #9

--- R/buffered_random_sampling.R
+++ R/buffered_random_sampling.R
@@ -10,8 +10,6 @@ buffered_random_sampling <- function() {
       selected = FALSE,
       selectable = TRUE,
       temp_selected = FALSE,
-      outside_buffdist_of_selected = TRUE,
-      outside_buffdist_of_temp_selected = TRUE
     )
 
   stratum <-
@@ -25,34 +23,31 @@ buffered_random_sampling <- function() {
     stratum_area <- stratum$area[i]
 
     buffering_distance <- sqrt((2*stratum_area) / (pi*stations_required_in_stratum))
-
+    element$selectable <- TRUE
+    element$temp_selected <- FALSE
     element_backup <- element # Used to restart if allocation fails for buffer distance
+    selected_elements <- element %>% filter(selected) %>% select(elementId) %>% unlist()
+    element <- update_selectable(element, selected_elements, buffering_distance)
 
     n_selected_inside_stratum <- 0
 
     repeat {
-      current_element <- element %>% filter(selectable) %>% sample_n(1) %>% select(elementId) %>% unlist()
+      random_selectable_element <- sample(which(element$selectable), 1)
 
-      element$current <- ifelse(element$elementId == current_element, TRUE, FALSE)
+      element$current <-
+        ifelse(1:nrow(element) == random_selectable_element, TRUE, FALSE)
 
-      # Remove all points in temp_not_selected that are within the buffering
-      # distance of the newly selected point. That way all new selected
-      # points are automatically within the allowed distance
-      distance_to_current_element <-
-        distHaversine(
-          p1 = element %>% filter(current) %>% select(longitude, latitude),
-          p2 = element %>% select(longitude, latitude)
-        )
-
+      # Mark current element as either selected or temp_selected
       if (element[which(element$current), "stratum"] == current_stratum) {
-        element[which(element$current), "selected"] <- TRUE
-        element[distance_to_current_element <= buffering_distance, "outside_buffdist_of_selected"] <- FALSE
-      } else {
-        element[which(element$current), "temp_selected"] <- TRUE
-        element[distance_to_current_element <= buffering_distance, "outside_buffdist_of_temp_selected"] <- FALSE
+        selection_column <- "selected"
+      } else{
+        selection_column <- "temp_selected"
       }
+      element[which(element$current), selection_column] <- TRUE
 
-      element$selectable <- element$outside_buffdist_of_selected & element$outside_buffdist_of_temp_selected
+      element <- update_selectable(element,
+                                   element[which(element$current), "elementId"],
+                                   buffering_distance)
 
       n_selected_inside_stratum <-
         element %>%
@@ -60,21 +55,38 @@ buffered_random_sampling <- function() {
         nrow()
 
       if (n_selected_inside_stratum == stations_required_in_stratum) {
+        # visualise(element)
+        # browser()
+
         break
       }
 
-      if (nrow(filter(element, selectable)) == 0) {
+      if ( element %>% filter(selectable & stratum == current_stratum) %>% nrow() == 0) {
         # Restart sampling with reduced buffering distance
-        buffering_distance <- buffering_distance * .9
         element <- element_backup
+
+        element$selectable <- TRUE
+        buffering_distance <- buffering_distance * .9
+
+        selected_elements <- element %>% filter(selected) %>% select(elementId) %>% unlist()
+        element <- update_selectable(element, selected_elements, buffering_distance)
       }
     }
-
-    # Enough stations have been selected. Cleanup before starting next stratum.
-    element$temp_selected <- FALSE
-    element$outside_buffdist_of_temp_selected <- TRUE
-    element$selectable <- element$outside_buffdist_of_selected & element$outside_buffdist_of_temp_selected
   }
+}
+
+update_selectable <- function(element, element_Ids, buffering_distance) {
+  for (element_Id in element_Ids) {
+    distance_to_selected_element <-
+      distHaversine(
+        p1 = element %>% filter(elementId == element_Id) %>% select(longitude, latitude),
+        p2 = element %>% filter(selectable) %>% select(longitude, latitude)
+      )
+
+    element[which(element$selectable), "selectable"] <-
+      distance_to_selected_element > buffering_distance
+  }
+  return(element)
 }
 
 visualise <- function(df) {

0a325c2c58bf3b331cd87faf168cc8e6de873be4
Johan Ejstrud <johan@ejstrud.com>
2021-09-09 13:44:27 -0200

    Add parameter to control level of feedback detail
    Close #27

--- R/buffered_random_sampling.R
+++ R/buffered_random_sampling.R
@@ -12,20 +12,24 @@
 #' @param verbose Print status while the algorithm is running.
 #' @param pause Stop the algorithm to inspect progress. Makes most sense when
 #' used together with \code{visualise} or \code{verbose}.
+#' @param detail Control how often \code{visualise}, \code{verbose}, and
+#' \code{pause} are shown, printed, or executed. Possible values 1, 2, 3, and 4,
+#' corresponding to finish, after each stratum, each time the algorithm is
+#' reset, or after every element.
 #'
 #' @return Data frame listing which stratum each element is associated with.
 #'
 #' @export
 #' @importFrom rlang .data
 buffered_random_sampling <- function(element, stratum, visualise=FALSE,
-                                     verbose=FALSE, pause=FALSE) {
+                                     verbose=FALSE, pause=FALSE, detail=2) {
   element <-
     element %>%
     dplyr::mutate(
       current = FALSE,
       selected = FALSE,
       selectable = TRUE,
-      temp_selected = FALSE,
+      temp_selected = FALSE
     )
 
   stratum <-
@@ -38,14 +42,16 @@ buffered_random_sampling <- function(element, stratum, visualise=FALSE,
     stations_required_in_stratum = stratum$n_stations[i]
     stratum_area <- stratum$area[i]
 
-    n_elements_in_stratum <- element %>% filter(stratum == "A") %>% nrow()
+    n_elements_in_stratum <- element %>% dplyr::filter(stratum == current_stratum) %>% nrow()
+
     if (stations_required_in_stratum > n_elements_in_stratum) {
       stop(glue::glue("There are fewer elements ({n_elements_in_stratum}) in ",
                       "stratum '{current_stratum}' than the required number of ",
-                      "stations ({stations_required_in_stratum}).")
+                      "stations ({stations_required_in_stratum}). Increase the ",
+                      "number of elements, or reduce the required number of ",
+                      "stations.")
            )
     }
-
 
     buffering_distance <- sqrt((2*stratum_area) / (pi*stations_required_in_stratum))
     element$selectable <- TRUE
@@ -59,11 +65,18 @@ buffered_random_sampling <- function(element, stratum, visualise=FALSE,
     element <- update_selectable(element, selected_elements, buffering_distance)
     element_backup <- element # Used to restart if allocation fails for buffer distance
 
-    if (isTRUE(verbose)) {
-      message(paste0("Allocating elements in stratum '", current_stratum, "' (",
-                     i, " of ", nrow(stratum), ")"))
+    if (detail >= 2) {
+      if (isTRUE(visualise)) visualise_allocation(element)
+      if (isTRUE(verbose)) {
+        message(glue::glue("Allocating elements in stratum '{current_stratum}'",
+                           "({i} of {nrow(stratum)})"))
+        if (detail >= 3) {
+          message(glue::glue("  Initial buffering distance is ",
+                                            "{round(buffering_distance)} m."))
+        }
+      }
+      if (isTRUE(pause)) wait_for_user_input()
     }
-    if (isTRUE(pause)) wait_for_user_input()
 
     while (n_selected_in_stratum(element, current_stratum) < stations_required_in_stratum) {
       current_element <- sample(which(element$selectable), 1)
@@ -83,20 +96,44 @@ buffered_random_sampling <- function(element, stratum, visualise=FALSE,
                                    element[which(element$current), "elementId"],
                                    buffering_distance)
 
+      if (detail >= 4) {
+        if (isTRUE(visualise)) visualise_allocation(element)
+        if (isTRUE(verbose)) {
+          message(glue::glue("    Selecting element ",
+                             "{element[which(element$current), 'elementId']}."))
+        }
+        if (isTRUE(pause)) wait_for_user_input()
+      }
+
       if (n_selectable_in_stratum(element, current_stratum) == 0) {
         # Restart sampling with reduced buffering distance
         element <- element_backup
         buffering_distance <- buffering_distance * .9
+
+        if (detail >= 3) {
+          if (isTRUE(visualise)) visualise_allocation(element)
+          if (isTRUE(verbose)) {
+            message(glue::glue("  No selectable elements left. Reducing ",
+                               "buffering distance to {round(buffering_distance)} m."))
+          }
+          if (isTRUE(pause)) wait_for_user_input()
+        }
       }
     }
-    if (isTRUE(visualise)) visualise_allocation(element)
+    if (detail >= 3) {
+      if (isTRUE(visualise)) visualise_allocation(element)
+      if (isTRUE(verbose)) message(paste0("  Finished allocation in stratum"))
+      if (isTRUE(pause)) wait_for_user_input()
+    }
   }
-
-  if (isTRUE(verbose)) message("Allocation finished.")
 
   element$current <- FALSE
   element$temp_selected <- FALSE
-  if (isTRUE(visualise)) visualise_allocation(element)
+
+  if (detail >= 1) {
+    if (isTRUE(verbose)) message("Allocation finished.")
+    if (isTRUE(visualise)) visualise_allocation(element)
+  }
 
   allocation <- element %>% dplyr::filter(selected) %>% dplyr::select(elementId, stratum)
   return(allocation)
